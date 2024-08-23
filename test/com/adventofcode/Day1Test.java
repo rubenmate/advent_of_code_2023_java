e34bc554f678f05753685a57f14e431f9cfb9049
Rubén Maté <79601967+rubenmate@users.noreply.github.com>
2024-08-23 13:04:48 +0200

    test: move test folder

--- test/com/adventofcode/Day1Test.java
+++ test/com/adventofcode/Day1Test.java
@@ -1,18 +1,23 @@
 package com.adventofcode;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day1Test {
+    Day1 day1;
+
+    @BeforeEach
+    public void setUp()  {
+        day1 = new Day1();
+    }
     @Test
     public void testPart1() {
-        Day1 day1 = new Day1();
         assertEquals(142, day1.runPart1(Day1.testFile1));
     }
     @Test
     public void testPart2() {
-        Day1 day1 = new Day1();
         assertEquals(281, day1.runPart2(Day1.testFile2));
     }
 }
