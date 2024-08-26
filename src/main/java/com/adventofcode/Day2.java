63328917af68d2bc56a52cc3ec2840b819d200ca
Rubén Maté <79601967+rubenmate@users.noreply.github.com>
2024-08-26 13:23:08 +0200

    feat: add day 2 part 1 and part 2

--- src/main/java/com/adventofcode/Day2.java
+++ src/main/java/com/adventofcode/Day2.java
@@ -13,38 +13,83 @@ public class Day2 {
 
     public static void main(String[] args){
         Day2 solution = new Day2();
-        var part1 = solution.runPart1();
+
+        var games = initializeGames();
+
+        var part1 = solution.runPart1(games);
+        var part2 = solution.runPart2(games);
 
         System.out.println("Day 2");
-        // System.out.println("Part 1: The result is " + part1);
-        // System.out.println("Part 2: The result is " + part2);
+        System.out.println("Part 1: The result is " + part1);
+        System.out.println("Part 2: The result is " + part2);
     }
 
-    int runPart1() {
-        List<String> lines = Util.readLines(testFile1);
+    int runPart1(List<Game> games) {
+        var result = 0;
+        for (Game game : games){
+            if (game.isPossible()) {
+                result += game.getId();
+            }
+        }
+        return result;
+    }
+
+    int runPart2(List<Game> games) {
+        var result = 0;
+        for (Game game : games){
+            var minimumRoll = game.minimumRoll();
+            var powers = minimumRoll.redCubes * minimumRoll.greenCubes * minimumRoll.blueCubes;
+            result += powers;
+        }
+        return result;
+    }
+
+    static List<Game> initializeGames() {
+
+        List<String> lines = Util.readLines(inputFile);
+
         Pattern gamePattern = Pattern.compile(".*:\\s");
+        Pattern rollsPattern = Pattern.compile("(\\d+)\\s(red|green|blue)");
+
         List<Game> games = new ArrayList<>();
         var i = 1;
         for (String line : lines) {
             Matcher matcher = gamePattern.matcher(line);
 
-          if (matcher.find()) {
-              String rollsString = matcher.replaceFirst("");
-              System.out.println(rollsString);
-          }
+            if (matcher.find()) {
+                Game game = new Game(i);
+                i++;
+                var rolls = game.getRolls();
 
-            Game game = new Game(i);
-            games.add(game);
-            i++;
+                String rollsString = matcher.replaceFirst("");
 
-            var rolls = game.getRolls();
-            rolls.add(new Roll(2, 3, 5));
-            rolls.add(new Roll(2, 3, 5));
+                String[] rollsStringSplit = rollsString.split(";");
+
+                for (String roll : rollsStringSplit) {
+                    Matcher rollMatcher = rollsPattern.matcher(roll);
+                    int red = 0, green = 0, blue = 0;
+                    while (rollMatcher.find()) {
+                        String color = rollMatcher.group(2);
+                        String number = rollMatcher.group(1);
+                        switch (color) {
+                            case "red":
+                                red += Integer.parseInt(number);
+                                break;
+                            case "green":
+                                green += Integer.parseInt(number);
+                                break;
+                            case "blue":
+                                blue += Integer.parseInt(number);
+                                break;
+                        }
+                    }
+                    rolls.add(new Roll(red, green, blue));
+                }
+
+                games.add(game);
+            }
         }
-        for (Game game : games){
-            // System.out.println(game);
-        }
-        return 0;
+        return games;
     }
 
 }
@@ -74,6 +119,31 @@ class Game {
         this.rolls = rolls;
     }
 
+    public boolean isPossible() {
+        boolean isPossible = true;
+        var rolls = this.rolls;
+        for (Roll roll : rolls) {
+            if (roll.redCubes > 12) { isPossible = false; break; }
+            else if (roll.greenCubes > 13) { isPossible = false; break; }
+            else if (roll.blueCubes > 14) { isPossible = false; break; }
+        }
+        return isPossible;
+    }
+
+    public Roll minimumRoll() {
+        int maxRed = 0;
+        int maxGreen = 0;
+        int maxBlue = 0;
+
+        for (Roll roll : this.rolls) {
+            maxRed = Math.max(maxRed, roll.redCubes);
+            maxGreen = Math.max(maxGreen, roll.greenCubes);
+            maxBlue = Math.max(maxBlue, roll.blueCubes);
+        }
+
+        return new Roll(maxRed, maxGreen, maxBlue);
+    }
+
     @Override
     public String toString() {
         StringBuilder rollString = new StringBuilder();
@@ -87,13 +157,13 @@ class Game {
 
 class Roll {
     int redCubes;
-    int blueCubes;
     int greenCubes;
+    int blueCubes;
 
-    public Roll(int redCubes, int blueCubes, int greenCubes) {
+    public Roll(int redCubes, int greenCubes, int blueCubes) {
         this.redCubes = redCubes;
-        this.blueCubes = blueCubes;
         this.greenCubes = greenCubes;
+        this.blueCubes = blueCubes;
     }
 
     @Override
